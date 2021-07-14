e9f1427e8c681aff09d1704db81f1172d017db09
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-07-14 12:23:30 +0900

    ActiveHashの実装

---
+++ app/models/item.rb
@@ -0,0 +1,26 @@
+class Item < ApplicationRecord
+  belongs_to :seller, class_name: "user"
+  has_one :log
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipment_date
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shippment_date_id
+  end
+
+end
