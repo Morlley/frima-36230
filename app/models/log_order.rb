11e543464c9c3fb25fe38e34ae68395410d45a3c
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-07-16 13:06:19 +0900

    バリデーションをより正しく設定

--- app/models/log_order.rb
+++ app/models/log_order.rb
@@ -3,17 +3,14 @@ class LogOrder
   attr_accessor :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :log_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)'}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
     validates :city
     validates :address_line1
-    validates :phone_number
+    validates :phone_number, length: { in: 10..11 , message: "is too short"}, format: { with: /\A\d{3}\d{4}\d{4}\z/, message: "is invalid. Input only number" }
     validates :user_id
     validates :item_id
-    validates :token
-  end
-
-  with_options numericality: { other_than: 1, message: "can't be blank"} do
-    validates :prefecture_id
   end
 
   def save
